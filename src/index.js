f0995a126b622f613c4d45a8e364dd465b6b7625
Nguyen Quoc Dai <nguyenquocdai2k1@gmail.com>
2023-02-15 23:45:47 +0700

    Refactor

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ const { path } = require('express/lib/application')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 //static file img
 app.use(express.static(path_var.join(__dirname,'public')))
 
 //HTTP logger
 app.use(morgan('combined'))
+
+//middleware
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json()) 
 
 //Template engine (handlebars)
 app.engine('hbs', handlebars.engine({
@@ -19,13 +27,12 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs')
 app.set('views', path_var.join(__dirname,'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+
+//Route init
+route(app)
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
