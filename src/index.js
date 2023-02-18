33cb198bdb6ce4360d447d3f3653d9ae4ac11cbc
Nguyen Quoc Dai <nguyenquocdai2k1@gmail.com>
2023-02-18 11:54:12 +0700

    install 3 thư vien

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
-const path_var = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const { path } = require('express/lib/application')
-const app = express()
-const port = 3000
+const path_var = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const { path } = require('express/lib/application');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
 //static file img
-app.use(express.static(path_var.join(__dirname,'public')))
+app.use(express.static(path_var.join(__dirname, 'public')));
 
 //HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //middleware
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.json()) 
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 //Template engine (handlebars)
-app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', path_var.join(__dirname,'resources/views'))
-
-
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path_var.join(__dirname, 'resources/views'));
 
 //Route init
-route(app)
+route(app);
 
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+          app.listen(port, () => {
+              console.log(`Example app listening on port ${port}`);
+          });
